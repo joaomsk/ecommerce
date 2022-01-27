550832fb94fa9ed0d699f4713720bf45f0beed18
João Dier <joao.dier@finplace.com.br>
2022-01-26 21:46:13 -0300

    sobe meu código para o git

---
+++ src/main/java/com/example/ecommercelp2/Ecommercelp2Application.java
@@ -0,0 +1,13 @@
+package com.example.ecommercelp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecommercelp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ecommercelp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecommercelp2/Ecommercelp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercelp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecommercelp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
